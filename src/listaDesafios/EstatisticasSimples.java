46bf5085a5d3f9f54adf7453589c894d4a4dc097
Joice-Simao <joice.simao@hotmail.com>
2023-02-07 16:23:39 -0300

    Resolução desafio Estatísticas Simples

--- src/listaDesafios/EstatisticasSimples.java
+++ src/listaDesafios/EstatisticasSimples.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
     Sua tarefa é processar uma seqüência de números inteiros para determinar as seguintes estatísticas:
         Valor mínimo
@@ -11,5 +13,39 @@
         Valor médio: 18.1666666
  */
 public class EstatisticasSimples {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = 0, valorMinimo = 999999999, valorMaximo = 0, totalNumeros = 0, valorMedio = 0,
+                somaNumeros= 0;
+
+        System.out.println("\t\t\t\t\t>>> Programa Estatística Simples <<<");
+        System.out.println("Informe os números que serão processados para finalizar o programa " +
+                "digite o número 0 (zero)\n");
+
+        do {
+            System.out.print("Informe um número : ");
+            numero = scanner.nextInt();
+
+            if (numero != 0) {
+                totalNumeros++;
+                somaNumeros = numero + somaNumeros;
+
+                if (numero < valorMinimo) {
+                    valorMinimo = numero;
+                }
+                if (numero > valorMaximo) {
+                    valorMaximo = numero;
+                }
+            }
+        }while (numero != 0);
+
+        valorMedio = somaNumeros / totalNumeros;
+
+        System.out.println("Valor mínimo: " +valorMinimo);
+        System.out.println("Valor máximo: " +valorMaximo);
+        System.out.println("Número de elementos na seqüência: " +totalNumeros);
+        System.out.println("Valor médio: " +valorMedio);
+
+    }
 }
